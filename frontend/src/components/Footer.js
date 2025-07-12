e1311bc8e6d908904510f7fceec919a282f2b10b
Pavan8023 <pavanpatil8023@gmail.com>
2025-07-12 22:38:34 +0530

    Add .nvmrc with supported Node.js version for Netlify

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -56,7 +56,7 @@ const Footer = () => {
         </div>
         
         <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
-          <p>&copy; {new Date().getFullYear()} Psyche Panacea. All rights reserved.</p>
+          <p>&copy; {new Date().getFullYear()} Threelancerr. All rights reserved.</p>
         </div>
       </div>
     </footer>
